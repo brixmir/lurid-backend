857b326a22504afaac5d1823d334c611a27505fa
brixmir <gamersolo112@gmail.com>
2026-04-01 16:29:43 +0300

    server.js

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.post("/submit-feedback", async (req, res) => {
     }
 });
 
-app.listen(10000, () => {
-    console.log("Server running");
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
